f5ef9130c07afa12dd738158961ba6784529497a
Noah Greff <greffnoah@gmail.com>
2023-01-31 07:49:58 +0100

    improved `PropertyType`

--- src/main/java/com/azalealibrary/configuration/property/PropertyType.java
+++ src/main/java/com/azalealibrary/configuration/property/PropertyType.java
@@ -1,184 +1,162 @@
 package com.azalealibrary.configuration.property;
 
 import com.azalealibrary.configuration.command.Arguments;
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.generator.WorldInfo;
 import org.bukkit.util.Vector;
 
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.UUID;
-import java.util.function.Function;
 
-public final class PropertyType<T> {
+@SuppressWarnings("unchecked")
+public class PropertyType<T> {
 
-    public static final PropertyType<String> STRING = new Builder<String>(String.class, "Text")
-            .parse((sender, arguments, currentValue) -> arguments.getLast())
-            .suggest((sender, arguments, currentValue) -> List.of("<text>"))
-            .done();
-    public static final PropertyType<Integer> INTEGER = new Builder<Integer>(Integer.class, "Whole number")
-            .parse((sender, arguments, currentValue) -> Integer.parseInt(arguments.getLast()))
-            .suggest((sender, arguments, currentValue) -> List.of("<number>"))
-            .done();
-    public static final PropertyType<Boolean> BOOLEAN = new Builder<Boolean>(Boolean.class)
-            .parse((sender, arguments, currentValue) -> {
-                String input = arguments.getLast();
-                if (!input.equals("true") && !input.equals("false")) {
-                    throw new RuntimeException();
-                }
-                return Boolean.parseBoolean(input);
-            })
-            .suggest((sender, arguments, currentValue) -> List.of(Boolean.toString(Boolean.FALSE.equals(currentValue))))
-            .done();
-    public static final PropertyType<Vector> VECTOR = new Builder<Vector>(Vector.class)
-            .parse((sender, arguments, currentValue) -> {
-                double x = arguments.find(0, "x", Double::parseDouble);
-                double y = arguments.find(1, "y", Double::parseDouble);
-                double z = arguments.find(2, "z", Double::parseDouble);
-                return new Vector(x, y, z);
-            })
-            .suggest((sender, arguments, currentValue) -> {
-                if (sender instanceof Player player) {
-                    Location location = player.getLocation();
-                    double x = location.getBlockX() + .5;
-                    double y = location.getBlockY() + .5;
-                    double z = location.getBlockZ() + .5;
-                    return List.of(x + " " + y + " " + z);
-                }
-                return List.of();
-            })
-            .done();
-    public static final PropertyType<Player> PLAYER = new Builder<Player>(Player.class)
-            .parse((sender, arguments, currentValue) -> (Player) sender)
-            .suggest((sender, arguments, currentValue) -> {
-                if (sender instanceof Player player) {
-                    return player.getWorld().getPlayers().stream().map(Player::getDisplayName).toList();
-                }
-                return List.of();
-            })
-            .print(Player::getDisplayName)
-            .serialize(player -> player.getUniqueId().toString())
-            .deserialize(uuid -> Bukkit.getPlayer(UUID.fromString((String) uuid)))
-            .done();
-    public static final PropertyType<World> WORLD = new Builder<World>(World.class)
-            .parse((sender, arguments, currentValue) -> Bukkit.getWorld(arguments.get(0)))
-            .suggest((sender, arguments, currentValue) -> Bukkit.getServer().getWorlds().stream().map(World::getName).toList())
-            .serialize(WorldInfo::getName)
-            .deserialize(name -> Bukkit.getWorld(name.toString()))
-            .print(WorldInfo::getName)
-            .done();
+    public static final PropertyType<String> STRING = new PropertyType<>(String.class, "text");
+    public static final PropertyType<Integer> INTEGER = new PropertyType<>(Integer.class) {
+        @Override
+        public Integer parse(CommandSender sender, Arguments arguments, @Nullable Integer currentValue) {
+            return Integer.parseInt(arguments.getLast());
+        }
+    };
+    public static final PropertyType<Float> FLOAT = new PropertyType<>(Float.class, "decimal") {
+        @Override
+        public Float parse(CommandSender sender, Arguments arguments, @Nullable Float currentValue) {
+            return Float.parseFloat(arguments.getLast());
+        }
+    };
+    public static final PropertyType<Boolean> BOOLEAN = new PropertyType<>(Boolean.class) {
+        @Override
+        public Boolean parse(CommandSender sender, Arguments arguments, @Nullable Boolean currentValue) {
+            if (!arguments.is(0, "true") && !arguments.is(0, "false")) {
+                throw new RuntimeException(); // ensure explicit "true" or "false" text has been provided
+            }
+            return Boolean.parseBoolean(arguments.getLast());
+        }
+
+        @Override
+        public List<String> complete(CommandSender sender, Arguments arguments, @Nullable Boolean currentValue) {
+            return List.of(Boolean.toString(Boolean.FALSE.equals(currentValue)));
+        }
+    };
+    public static final PropertyType<Vector> VECTOR = new PropertyType<>(Vector.class) {
+        @Override
+        public List<String> complete(CommandSender sender, Arguments arguments, @Nullable Vector currentValue) {
+            if (sender instanceof Player player) {
+                Location location = player.getLocation();
+                double x = location.getBlockX() + .5;
+                double y = location.getBlockY() + .5;
+                double z = location.getBlockZ() + .5;
+                return List.of(x + " " + y + " " + z);
+            }
+            return super.complete(sender, arguments, currentValue);
+        }
+
+        @Override
+        public Vector parse(CommandSender sender, Arguments arguments, @Nullable Vector currentValue) {
+            double x = arguments.find(0, "x", Double::parseDouble);
+            double y = arguments.find(1, "y", Double::parseDouble);
+            double z = arguments.find(2, "z", Double::parseDouble);
+            return new Vector(x, y, z);
+        }
+    };
+    public static final PropertyType<Player> PLAYER = new PropertyType<>(Player.class) {
+        @Override
+        public List<String> complete(CommandSender sender, Arguments arguments, @Nullable Player currentValue) {
+            if (sender instanceof Player player) {
+                return player.getWorld().getPlayers().stream().map(Player::getDisplayName).toList();
+            }
+            return super.complete(sender, arguments, currentValue);
+        }
+
+        @Override
+        public Player parse(CommandSender sender, Arguments arguments, @Nullable Player currentValue) {
+            return (Player) sender;
+        }
+
+        @Override
+        public Object serialize(Player object) {
+            return object.getUniqueId().toString();
+        }
+
+        @Override
+        public Player deserialize(Object object) {
+            return Bukkit.getPlayer(UUID.fromString((String) object));
+        }
+
+        @Override
+        public String print(Player object) {
+            return object.getDisplayName();
+        }
+    };
+
+    public static final PropertyType<World> WORLD = new PropertyType<>(World.class) {
+        @Override
+        public List<String> complete(CommandSender sender, Arguments arguments, @Nullable World currentValue) {
+            return Bukkit.getServer().getWorlds().stream().map(World::getName).toList();
+        }
+
+        @Override
+        public World parse(CommandSender sender, Arguments arguments, @Nullable World currentValue) {
+            return Bukkit.getWorld(arguments.getLast());
+        }
+
+        @Override
+        public Object serialize(World object) {
+            return object.getName();
+        }
+
+        @Override
+        public World deserialize(Object object) {
+            return Bukkit.getWorld((String) object);
+        }
+
+        @Override
+        public String print(World object) {
+            return object.getName();
+        }
+    };
 
     private final Class<?> type;
     private final String expected;
-    private final Parser<T> parser;
-    private final Completer<T> completer;
-    private final Function<T, String> toString;
-    private final Function<T, Object> toObject;
-    private final Function<Object, T> toValue;
 
-    private PropertyType(Class<?> type, String expected, Parser<T> parser, Completer<T> completer, Function<T, String> toString, Function<T, Object> toObject, Function<Object, T> toValue) {
-        this.expected = expected;
-        this.type = type;
-        this.parser = parser;
-        this.completer = completer;
-        this.toString = toString;
-        this.toObject = toObject;
-        this.toValue = toValue;
+    protected PropertyType(Class<?> type) {
+        this(type, type.getSimpleName());
     }
 
-    public Class<?> getType() {
+    protected PropertyType(Class<?> type, String expected) {
+        this.expected = expected;
+        this.type = type;
+    }
+
+    public final Class<?> getType() {
         return type;
     }
 
-    public String getExpected() {
+    public final String getExpected() {
         return expected;
     }
 
+    public List<String> complete(CommandSender sender, Arguments arguments, @Nullable T currentValue) {
+        return List.of("<" + expected + ">");
+    }
+
     public T parse(CommandSender sender, Arguments arguments, @Nullable T currentValue) {
-        try {
-            return parser.parse(sender, arguments, currentValue);
-        } catch (Exception exception) {
-            throw new RuntimeException(expected + " expected. Got " + arguments);
-        }
+        return (T) arguments.getLast();
     }
 
-    public List<String> suggest(CommandSender sender, Arguments arguments, @Nullable T currentValue) {
-        return completer.complete(sender, arguments, currentValue);
+    public Object serialize(T object) {
+        return object;
     }
 
-    public String toString(T value) {
-        return toString.apply(value);
+    public T deserialize(Object object) {
+        return (T) object;
     }
 
-    public Object toObject(T value) {
-        return toObject.apply(value);
-    }
-
-    public T toValue(Object object) {
-        return toValue.apply(object);
-    }
-
-    public static class Builder<T> {
-
-        private final Class<?> type;
-        private final String expected;
-        private Parser<T> parser = (sender, arguments, currentValue) -> (T) arguments.getLast();
-        private Completer<T> completer = (sender, arguments, currentValue) -> List.of();
-        private Function<T, String> toString = Object::toString;
-        private Function<T, Object> toObject = value -> value;
-        private Function<Object, T> toValue = object -> (T) object;
-
-        public Builder(Class<?> type) {
-            this(type, type.getSimpleName());
-        }
-
-        public Builder(Class<?> type, String expected) {
-            this.type = type;
-            this.expected = StringUtils.capitalize(expected);
-        }
-
-        public Builder<T> parse(Parser<T> parser) {
-            this.parser = parser;
-            return this;
-        }
-
-        public Builder<T> suggest(Completer<T> completer) {
-            this.completer = completer;
-            return this;
-        }
-
-        public Builder<T> print(Function<T, String> function) {
-            this.toString = function;
-            return this;
-        }
-
-        public Builder<T> serialize(Function<T, Object> function) {
-            this.toObject = function;
-            return this;
-        }
-
-        public Builder<T> deserialize(Function<Object, T> function) {
-            this.toValue = function;
-            return this;
-        }
-
-        public PropertyType<T> done() {
-            return new PropertyType<>(type, expected, parser, completer, toString, toObject, toValue);
-        }
-    }
-
-    @FunctionalInterface
-    public interface Parser<T> {
-        T parse(CommandSender sender, Arguments arguments, @Nullable T currentValue);
-    }
-
-    @FunctionalInterface
-    public interface Completer<T> {
-        List<String> complete(CommandSender sender, Arguments arguments, @Nullable T currentValue);
+    public  String print(T object) {
+        return object.toString();
     }
 }
