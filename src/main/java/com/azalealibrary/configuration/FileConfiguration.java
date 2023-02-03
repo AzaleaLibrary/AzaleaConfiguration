8f2781f2830ceb2e540c58d70f6122a82c566f06
Noah Greff <greffnoah@gmail.com>
2023-02-03 08:00:41 +0100

    simplified `FileConfiguration`

--- src/main/java/com/azalealibrary/configuration/FileConfiguration.java
+++ src/main/java/com/azalealibrary/configuration/FileConfiguration.java
@@ -3,7 +3,7 @@ package com.azalealibrary.configuration;
 import com.azalealibrary.configuration.property.ConfigurableProperty;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.util.List;
@@ -11,18 +11,22 @@ import java.util.logging.Level;
 
 public class FileConfiguration {
 
-    private final String name;
-    private final JavaPlugin plugin;
+    private final Plugin plugin;
     private final Configurable configurable;
+    private final File file;
 
-    public FileConfiguration(String name, JavaPlugin plugin, Configurable configurable) {
-        this.name = name;
+    public FileConfiguration(Plugin plugin, Configurable configurable, String name) {
+        this(plugin, configurable, new File(plugin.getDataFolder(), name + ".yml"));
+    }
+
+    public FileConfiguration(Plugin plugin, Configurable configurable, File file) {
         this.plugin = plugin;
         this.configurable = configurable;
+        this.file = file;
     }
 
     public String getName() {
-        return name;
+        return file.getName().substring(0, file.getName().lastIndexOf('.'));
     }
 
     public Configurable getConfigurable() {
@@ -30,11 +34,7 @@ public class FileConfiguration {
     }
 
     public void load() {
-        loadFromFile(getConfigFile());
-    }
-
-    public void loadFromFile(File file) {
-        plugin.getLogger().log(Level.INFO, "Loading '" + name + "' data.");
+        plugin.getLogger().log(Level.INFO, "Loading '" + getName() + "' data.");
         YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
 
         try {
@@ -42,16 +42,12 @@ public class FileConfiguration {
                 property.deserialize(config);
             }
         } catch (Exception exception) {
-            plugin.getLogger().log(Level.WARNING, exception, () -> "Could not load '" + name + "' data.");
+            plugin.getLogger().log(Level.WARNING, exception, () -> "Could not load '" + getName() + "' data.");
         }
     }
 
     public void save() {
-        saveToFile(getConfigFile());
-    }
-
-    public void saveToFile(File file) {
-        plugin.getLogger().log(Level.INFO, "Saving '" + name + "' data.");
+        plugin.getLogger().log(Level.INFO, "Saving '" + getName() + "' data.");
         YamlConfiguration config = new YamlConfiguration();
 
         try {
@@ -62,33 +58,20 @@ public class FileConfiguration {
             }
             config.save(file);
         } catch (Exception exception) {
-            plugin.getLogger().log(Level.WARNING, exception, () -> "Could not save '" + name + "'.");
+            plugin.getLogger().log(Level.WARNING, exception, () -> "Could not save '" + getName() + "'.");
         }
-    }
-
-    private File getConfigFile() {
-        File file = new File(plugin.getDataFolder(), name + ".yml");
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (Exception exception) {
-                plugin.getLogger().log(Level.WARNING, exception, () -> "Could not create file for configuration '" + name + "'.");
-            }
-        }
-        return file;
     }
 
     @Override
     public boolean equals(Object object) {
         if (object instanceof FileConfiguration fileConfiguration) {
-            return name.equals(fileConfiguration.name) && plugin.equals(fileConfiguration.plugin);
+            return file.equals(fileConfiguration.file) && plugin.equals(fileConfiguration.plugin);
         }
         return super.equals(object);
     }
 
     @Override
     public String toString() {
-        return String.format("name:%s, owner:%s", name, plugin.getName());
+        return String.format("path:%s, plugin:%s", file.getPath(), plugin.getName());
     }
 }
