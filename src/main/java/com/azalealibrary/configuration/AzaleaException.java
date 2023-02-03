d1b662c11021b3df49ae0466ad309f5d0ef4cad7
Noah Greff <greffnoah@gmail.com>
2023-02-03 02:00:38 +0100

    added `AzaleaException`

---
+++ src/main/java/com/azalealibrary/configuration/AzaleaException.java
@@ -0,0 +1,19 @@
+package com.azalealibrary.configuration;
+
+public class AzaleaException extends RuntimeException {
+
+    private final String[] messages;
+
+    public AzaleaException() {
+        messages = new String[0];
+    }
+
+    public AzaleaException(String message, String... messages) {
+        super(message);
+        this.messages = messages;
+    }
+
+    public String[] getMessages() {
+        return messages;
+    }
+}
