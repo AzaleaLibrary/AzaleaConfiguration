2a3e3bc61db5af6f9ddc117bc8e14f441f3157ad
Noah Greff <greffnoah@gmail.com>
2023-03-07 18:24:05 +0100

    fixed legacy message

--- src/main/java/net/azalealibrary/configuration/AzaleaConfiguration.java
+++ src/main/java/net/azalealibrary/configuration/AzaleaConfiguration.java
@@ -5,12 +5,14 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 import org.bukkit.plugin.java.annotation.dependency.Dependency;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
 import java.io.File;
 
 @SuppressWarnings("unused")
 @Plugin(name = "AzaleaConfiguration", version = "1.0")
+@ApiVersion(ApiVersion.Target.v1_19)
 @Dependency("AzaleaCommand")
 public final class AzaleaConfiguration extends JavaPlugin {
 
