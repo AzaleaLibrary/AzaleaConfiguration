35948441f533523b64ffa2a019c221bad8dc4c61
noahgreff <greffnoah@gmail.com>
2024-12-08 19:49:20 +0100

    added `ConfigProperty`

--- src/main/java/net/azalealibrary/configuration/Configuration.java
+++ src/main/java/net/azalealibrary/configuration/Configuration.java
@@ -1,48 +1,96 @@
 package net.azalealibrary.configuration;
 
 import net.azalealibrary.command.AzaleaException;
+import net.azalealibrary.command.TextUtil;
+import net.azalealibrary.configuration.annotation.ConfigProperty;
 import net.azalealibrary.configuration.property.ConfigurableProperty;
+import net.azalealibrary.configuration.property.ListProperty;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
-public class Configuration implements Configurable {
+public abstract class Configuration {
 
+    private final JavaPlugin plugin;
     private final String name;
-    private final List<ConfigurableProperty<?, ?>> properties;
+    private final File file;
+    private final YamlConfiguration configuration;
 
-    public Configuration(String name) {
+    public Configuration(JavaPlugin plugin, String name) {
+        this.plugin = plugin;
         this.name = name;
-        this.properties = new ArrayList<>();
+        this.file = getFile();
+        this.configuration = YamlConfiguration.loadConfiguration(file);
     }
 
-    @Override
     public String getName() {
         return name;
     }
 
-    @Override
-    public List<ConfigurableProperty<?, ?>> getProperties() {
-        return properties;
-    }
+    public void load() {
+        try {
+            plugin.getLogger().log(Level.INFO, "Loading '" + getName() + "' data.");
 
-    public void merge(Configurable configurable) {
-        if (!configurable.getName().equals(name)) {
-            throw new AzaleaException(String.format("Attempting to merge incompatible configurations '%s' into '%s'.", configurable.getName(), name));
-        }
-
-        for (ConfigurableProperty<?, ?> property : configurable.getProperties()) {
-            if (properties.contains(property)) {
-                throw new AzaleaException(String.format("Attempting to merge configuration with existing property: %s", property.getName()));
+            for (ConfigurableProperty<?, ?> property : getProperties()) {
+                property.deserialize(configuration);
             }
-            properties.add(property);
+        } catch (Exception exception) {
+            plugin.getLogger().log(Level.WARNING, exception, () -> "Could not load '" + getName() + "' data.");
         }
     }
 
-    public void unmerge(Configurable configurable) {
-        if (!configurable.getName().equals(name)) {
-            throw new AzaleaException(String.format("Attempting to unmerge incompatible configurations '%s' into '%s'.", configurable.getName(), name));
+    public void save() {
+        try {
+            plugin.getLogger().log(Level.INFO, "Saving '" + getName() + "' data.");
+
+            for (ConfigurableProperty<?, ?> property : getProperties()) {
+                property.serialize(configuration);
+                List<String> comments = new ArrayList<>();
+                String type = property.getPropertyType().getExpected() + (property instanceof ListProperty<?> ? " (list)" : "");
+                comments.add("Property: " + property.getName() + " of " + type);
+                comments.add("Default: " + property.getDefault());
+                property.getDescription().forEach(l -> comments.addAll(TextUtil.split(l, 55).stream().map(i -> "  " + i).toList()));
+                configuration.setComments(property.getName(), comments);
+            }
+            configuration.save(file);
+        } catch (Exception exception) {
+            plugin.getLogger().log(Level.WARNING, exception, () -> "Could not save '" + getName() + "'.");
         }
-        properties.removeAll(configurable.getProperties());
+    }
+
+    private File getFile() {
+        try {
+            File file = new File(plugin.getDataFolder(), getName() + ".yml");
+
+            if (!plugin.getDataFolder().exists() && plugin.getDataFolder().mkdir() && !file.exists()) {
+                file.createNewFile();
+                save(); // set initial config data
+            }
+            return file;
+        } catch (Exception exception) {
+            throw new AzaleaException("Could not create configuration file '" + file + "'.", exception);
+        }
+    }
+
+    public List<ConfigurableProperty<?, ?>> getProperties() {
+        List<ConfigurableProperty<?, ?>> properties = new ArrayList<>();
+
+        for (Field field : getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(ConfigProperty.class)) {
+                try {
+                    field.setAccessible(true);
+
+                    if (field.get(this) instanceof ConfigurableProperty<?, ?> property) {
+                        properties.add(property);
+                    }
+                } catch (IllegalAccessException ignored) { }
+            }
+        }
+        return properties;
     }
 }
---
+++ src/main/java/net/azalealibrary/configuration/annotation/ConfigProperty.java
@@ -0,0 +1,7 @@
+package net.azalealibrary.configuration.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConfigProperty { }
